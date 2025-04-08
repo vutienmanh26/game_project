35af97b8240efe4819e5b0ffbc60f5091974b2b6
vutienmanh26 <24020226@vnu.edu.vn>
2025-04-08 19:29:47 +0700

    update

--- Game.h
+++ Game.h
@@ -28,11 +28,11 @@ enum GameState {
 
 class Game {
 private:
-    // Các thành phần SDL
+
     SDL_Window* window;
     SDL_Renderer* renderer;
 
-    // Các texture
+
     std::map<int, SDL_Texture*> textures;
     SDL_Texture* menuTexture;
     SDL_Texture* winTexture;
@@ -41,10 +41,10 @@ private:
     SDL_Texture* startButtonTexture;
     SDL_Texture* restartButtonTexture;
 
-    // Âm thanh
+
     Mix_Music* backgroundMusic;
 
-    // Trạng thái game
+
     GameState gameState;
     int flipCount;
     int score;
@@ -56,12 +56,12 @@ private:
     void saveHighScore();
 
 
-    // Lưới game
+
     std::vector<std::vector<int>> grid;
     std::vector<std::vector<bool>> revealed;
     std::vector<std::vector<bool>> matched;
 
-    // Các ô đã click
+
     std::pair<int, int> firstClick;
     std::pair<int, int> secondClick;
 
@@ -69,35 +69,33 @@ public:
     Game();
     ~Game();
 
-    // Khởi tạo và quản lý game
+
     bool initSDL();
     void loadResources();
     void initializeGrid();
     void closeSDL();
 
-    // Xử lý game
     void handleMouseClick(int x, int y);
     void updateGameState();
     bool checkRestartButtonClick(int x, int y);
 
-    // Render
+
     void renderGrid();
     void renderMenu();
     void renderResult(bool isWin);
-    void renderTimer(Uint32 remainingTime);
     void renderScore();
     void renderTime();
     void renderRestartButton();
     void renderHighScore();
 
-    // Âm thanh
+
     void playBackgroundMusic();
     void stopBackgroundMusic();
 
-    // Tiện ích
+
     SDL_Texture* loadTexture(const std::string& path);
 
-    // Getter cho gameState để kiểm tra trạng thái từ bên ngoài
+
     GameState getGameState() const { return gameState; }
     SDL_Renderer* getRenderer() const { return renderer; }
 };
